daaa038087a32fe5123168bac59c65c48602475f
16467 <sample@bths.edu>
2023-09-23 15:56:28 -0400

    Finished the freestyle portion where I add a gratuity for groups of 6 or more.

--- src/main.java
+++ src/main.java
@@ -5,6 +5,9 @@ public class main {
         System.out.println("Welcome to the helpful tip calculator! ");
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Keep in mind that if you have a group of 6 or more, you will have to automatically have to pay an extra 18% of the check without the tip for gratuity.");
+        System.out.println("<-------------------------------------------------------------------->");
+
         System.out.print("How many people are here with you today? ");
         int group = scan.nextInt();
         scan.nextLine();
@@ -25,6 +28,8 @@ public class main {
             tempCost = scan.nextDouble();
             totalCost += tempCost;
         }
+
+        //  I learned how to round to the nearest hundredth using DecimalFormatter through https://www.baeldung.com/java-round-decimal-number
 
         // finding total cost without tip
         double totalNewCost = ((totalCost + cost) + 1);
@@ -59,5 +64,15 @@ public class main {
         System.out.println("Per person cost before tip: $" + z.format(personWithoutTip));
         System.out.println("Tip per person: $" + t.format(tipPerPerson));
         System.out.println("Total cost per person: $" + w.format(totalPerson));
+
+        // calculating 18 percent of the check for those who have groups of 6 or more
+        if (group >= 6) {
+            double gratuity = totalNewCost * 0.18;
+            DecimalFormat k = new DecimalFormat("##.00");
+            double newTotalForSixOrMore = totalBill + gratuity;
+            DecimalFormat h = new DecimalFormat("##.00");
+            System.out.println("Total gratuity for groups of 6 or more: $" + k.format(gratuity));
+            System.out.println("Total cost with tip and gratuity: $" + h.format(newTotalForSixOrMore));
+        }
     }
 }
